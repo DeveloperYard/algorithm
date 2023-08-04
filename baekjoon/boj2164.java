b279c279191134e094bf5878075c4576052d4b72
Seungwoo Kim <wskyard96@gmail.com>
2023-08-04 16:06:22 +0900

    Add files via upload
    more optimizing, don't using ArrayList, must use Queue(LinkedList)

--- baekjoon/boj2164.java
+++ baekjoon/boj2164.java
@@ -1,36 +1,29 @@
-package java_boj;
+package com.ssafy.corona.app;
 
 import java.util.*;
 
-
-public class boj2164 {
-	static LinkedList<String> cardl = new LinkedList<>();
-	static Queue<String> cardq = new LinkedList<>();
+public class Main_백준_2164_카드2_김승우_180ms {
 	
+	static LinkedList<Integer> arr = new LinkedList<>();
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
 		int n = sc.nextInt();
-		for(int i = 1; i <= n; i++ ) {
-			cardq.add(i + "");
+		for(int i = 0; i<n; i++) {
+			arr.add(i+1); // O(n)
 		}
-		
-		bruteForce();
-		System.out.println(cardq.peek());
+
+		card_method();
+
+		System.out.println(arr.get(0));
 	}
-	private static void bruteForce() {
-//		while (cardl.size() > 1) {
-//			cardl.removeFirst();
-//			String s = cardl.getFirst();
-//			cardl.addLast(s);
-//			cardl.removeFirst();
-//		}
-		while (cardq.size() > 1) {
-			cardq.remove();
-			cardq.add(cardq.peek());
-			cardq.remove();
+	
+	private static void card_method() { // O(n)
+		while(arr.size() > 1) {
+			arr.remove();
+			arr.add(arr.peek());
+			arr.remove();
 		}
-		
-		return;
 	}
 }
