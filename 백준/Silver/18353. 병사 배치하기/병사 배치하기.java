bd2193704ccfb6ac496238493642aa9c9261cbd9
Seungwoo Kim <wskyard96@gmail.com>
2023-08-16 13:40:00 +0900

    [Silver II] Title: 병사 배치하기, Time: 108 ms, Memory: 12324 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
@@ -1,47 +1,54 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int[] soldiers;
-    static int[] dp;
-    static int N;
-    static int byeSoldiers;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
-        soldiers = new int[N];
-        dp = new int[N];
-
-        for(int i = 0; i<N; i++){
-            dp[i] = 1;
-        }
-        st = new StringTokenizer(br.readLine(), " ");
-        for(int i = 0; i < N; i++){
-            soldiers[i] = Integer.parseInt(st.nextToken());
-        }
-
-        decreasingSequence();
-        for(int e : dp){
-            if (e > byeSoldiers){
-                byeSoldiers = e;
-            }
-        }
-        //System.out.println(Arrays.toString(dp));
-        System.out.println(N - byeSoldiers);
-
-    }
-
-    private static void decreasingSequence(){
-        for(int i = 1; i < N; i++){
-            int j = 1;
-            while (i >= j){
-                if(soldiers[i-j] > soldiers[i]){
-                    dp[i] = Math.max(dp[i-j]+1, dp[i]);
-                }
-                j++;
-            }
-        }
-    }
+	
+	static int N;
+	static List<Integer> soldiers = new ArrayList<>();
+	static ArrayList<Integer> vc = new ArrayList<>();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i < N; i++) {
+			soldiers.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		Collections.reverse(soldiers);
+		
+		for(int i = 0; i < N; i++) {
+			if(vc.isEmpty()) {
+				vc.add(soldiers.get(i));
+			} else {
+				if(vc.get(vc.size()-1) < soldiers.get(i)) {
+					vc.add(soldiers.get(i));
+				} else {
+					vc.set(lower_bound(soldiers.get(i)), soldiers.get(i));
+				}
+			}
+		}
+		System.out.println(soldiers.size()-vc.size());
+	}
+	
+	public static int lower_bound(int target) {
+		int start = 0;
+		int end = vc.size()-1;
+		int index = 0;
+		
+		while(start <= end) {
+			int mid = (start + end)/2;
+			if(vc.get(mid) >= target) {
+				end = mid-1;
+				index = mid;
+			} else {
+				start = mid+1;
+			}
+		}
+		
+		return index;
+	}
 }
