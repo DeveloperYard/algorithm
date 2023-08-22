d7b86d7a341db6f3aeb24ba572622502eeb499e6
Seungwoo Kim <wskyard96@gmail.com>
2023-08-22 10:56:55 +0900

    [Silver III] Title: 악수, Time: 192 ms, Memory: 51912 KB -BaekjoonHub

---
+++ 백준/Silver/8394. 악수/악수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+	
+	static int[] dp;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		dp = new int[10000001];
+		
+		dp[0] = 1;
+		dp[1] = 1;
+		
+		for(int i = 2; i <= n; i++) {
+			dp[i] = dp[i-1]+dp[i-2];
+			dp[i]%=10;
+		}
+		System.out.println(dp[n]);
+		
+		
+	}
+}
