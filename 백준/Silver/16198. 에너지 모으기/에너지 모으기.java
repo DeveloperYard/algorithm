aaedff441e3e7c76bcaae1bc597f21d942dbe587
Seungwoo Kim <wskyard96@gmail.com>
2023-08-27 15:33:09 +0900

    [Silver I] Title: 에너지 모으기, Time: 128 ms, Memory: 15668 KB -BaekjoonHub

---
+++ 백준/Silver/16198. 에너지 모으기/에너지 모으기.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+    static long max = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        ArrayList<Integer> a = new ArrayList<>();
+
+        for(int i = 0; i < N; i++){
+            a.add(Integer.parseInt(st.nextToken()));
+        }
+
+        getEnergy(N, 0, a);
+        System.out.println(max);
+    }
+
+    private static void getEnergy(int n, long curSum, ArrayList<Integer> paramArr){
+        if(n == 2){
+            if(max < curSum) max = curSum;
+            return;
+        }
+
+        for(int i = 1; i < paramArr.size()-1; i++){
+            int tmp = paramArr.get(i);
+            int energy = paramArr.get(i-1)*paramArr.get(i+1);
+            paramArr.remove(i);
+            getEnergy(n-1, curSum+energy, paramArr);
+            paramArr.add(i, tmp);
+
+        }
+    }
+
+
+}
